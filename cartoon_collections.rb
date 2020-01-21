05926bfb00cfd388afd7aa08d173ad6f34915bfb
Phillip Henry III <philtrader71@gmail.com>
2020-01-21 05:19:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)# code an argument here
+planeteer_calls.any? { |call| call.length > 4 }  # Your code here
 end
 
 def find_the_cheese# code an argument here
