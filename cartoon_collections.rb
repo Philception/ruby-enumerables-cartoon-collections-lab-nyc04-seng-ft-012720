3c8b4261d94ee85398e43a5c176e8ce4c390f3ec
Phillip Henry III <philtrader71@gmail.com>
2020-01-21 05:21:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,8 +27,14 @@ end
 def long_planeteer_calls(planeteer_calls)# code an argument here
 planeteer_calls.any? { |call| call.length > 4 }  # Your code here
 end
+long_planeteer_calls(planeteer_calls)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
+
+find_the_cheese(potentially_cheesy_items)
